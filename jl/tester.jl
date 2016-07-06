73f66866b16f9228bf9eb665d39c441a04b65e77
Thomas Delaney <thomas_delaney@edinburghairport.com>
2016-07-06 10:34:12 +0100

    Cleaned up the tester file.

--- jl/tester.jl
+++ jl/tester.jl
@@ -25,37 +25,10 @@ method = "plugin" # will need more eventually
 sampling = "naive"
 calc = ["HX", "HXY"]
 
-# initialising type constants
 x = response
 y = stimulus
 ydims = [length(y), 1 + maximum(y)]
 xdims = [length(x), 1 + maximum(x)]
-X_n = X_dimensions[1]
-X_m = X_dimensions[2]
-Y_n = Y_dimensions[1]
-Y_m = Y_dimensions[2]
-X_dimension = X_m ^ X_n
-Y_dimension = Y_m ^ Y_n
-N = X_m
-Ny = zeros(Int, Y_dimension)
 
-# allocating memory for requested calculations
-PY = zeros(Y_dimension) # will hold the Y probabilities eventually
-PX = zeros(X_dimension) # will hold the X probabilities eventually
-PXY = zeros(X_dimension, Y_dimension)
-
-d_X = X # I think this is supposed to be a 1D version of X
-d_Y = Y # ''
-
-PX = prob(d_X, X_m)
-PY = prob(d_Y, Y_m)
-Ny, PXY = condprob(d_X, d_Y, X_dimension, Y_dimension)
-
-HX = entropy(PX, 2)
-HY = entropy(PY, 2)
-HXY = condEntropy(PY, PXY, 2)
-I = HX - HXY
-
-# need to start writing the actual function here.
-# there's a function in the 'StatsBase' package named entropy
-# I need to read about this
+ent_dict = calcentropy(x, y, xdims, ydims, calc, method, sampling)
+I = ent_dict["HX"] - ent_dict["HXY"]
